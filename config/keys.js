023005474483e675bae5805a56a5b6ec230f079e
Nicu Mardari <nickeone@Igors-MacBook-Pro-2.local>
2021-05-06 21:32:27 +0300

    google-oatuh

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+   googleClientID: '868798256085-inukkcfl5um9b6hpc8dogdrcujse07l3.apps.googleusercontent.com',
+   googleClientSecret: 'YW3wVO_8JRxOzCCxoApgWzjN'
+}
--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js')
+
 const app = express();
 
+//https://console.cloud.google.com/
+
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+        },
+  (accessToken) => {
+            console.log(accessToken);
+        }
+    )
+);
 
 
-app.get('/',(req, res) => {
-    console.log('req', req);
-    console.log('res', res);
 
-    res.send({Hi: 'Nicu'
-    });
-
-})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
