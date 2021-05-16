7d9cda548110fd3bb06326b40eb000ef99322cad
Nicu Mardari <nickeone@Igors-MacBook-Pro-2.local>
2021-05-16 14:42:06 +0300

    heroku fix keys

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@
 // dev.js don't commit this!!!
 
 if(process.env.NODE_ENV === 'production'){
-    console.log('rocess.env.NODE_ENV:', rocess.env.NODE_ENV);
+    // console.log('rocess.env.NODE_ENV:', process.env.NODE_ENV);
 // we are in production - return prod set of keys
 module.exports = require('./prod');
 }else {
