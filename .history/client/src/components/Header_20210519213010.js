26585feda8cf325872e88df9e64a34172cf7affc
Nicu Mardari <nickeone@Igors-MacBook-Pro-2.local>
2021-05-19 21:34:14 +0300

    83 Header design

--- .history/client/src/components/Header_20210519213010.js
+++ .history/client/src/components/Header_20210519213010.js
@@ -8,7 +8,7 @@ class Header extends Component {
               <a href="#" className="brand-logo">Logo</a>
               <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><a href="sass.html">Login with Google</a></li>
-                <li><a href="badges.html">Login with Facebook</a></li>
+                <li><a href="badges.html">Components</a></li>
               </ul>
             </div>
           </nav>
